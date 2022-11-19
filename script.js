0e4ccacc8af064f087f6a9170e8a0d24210391f1
Ayush Agarwal <aa2503452@gmail.com>
2022-11-19 07:11:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+function newQuote() {
+  showLoadingSpinner();
+  // pick a random quote from apiQutoes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // checking if author field is null then display "Unknown"
+  if (quote.author === null) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  removeLoadingSpinner();
+}
+
+// Get Quotes From API
+let counter = 0;
+async function getQutoes() {
+  showLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch Error Here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+getQutoes();
